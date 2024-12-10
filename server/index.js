310f3f53ee3c2a7ec280a0aef2b27ba8f737b5f4
ZAIN-UL-ABEDIN <zainhanif2902@gmail.com>
2024-12-10 14:54:17 +0500

    backend setup & register and login api

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+
+dotenv.config({});
+const app = express();
+// Middleware
+app.use(express.json());
+app.use(cors({}));
+app.use(cookieParser());
+// API
+app.use("/api/v1/user", userRoute);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listening at port ${PORT}`);
+});
