d5710151c548816f281ce389c92e1b3275f1b1bf
ZainHanif29 <zainhanif2902@gmail.com>
2024-12-19 20:28:31 +0500

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login";
-import Navbar from "./components/Navbar";
+import MainLayout from "./Layout/MainLayout";
 import HeroSection from "./pages/student/HeroSection";
 
 const App = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <>
+              <HeroSection />
+            </>
+          ),
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
   return (
     <>
-      <main>
-        <Navbar />
-        <HeroSection />
-        <Login />
-      </main>
+      <RouterProvider router={appRouter} />
     </>
   );
 };
