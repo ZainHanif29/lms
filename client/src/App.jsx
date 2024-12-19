7783e4d43531a0603281117293d5bc73aad7fbcf
ZainHanif29 <zainhanif2902@gmail.com>
2024-12-19 19:01:32 +0500

    Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Login from "./pages/login";
 import Navbar from "./components/Navbar";
+import HeroSection from "./pages/student/HeroSection";
 
 const App = () => {
   return (
     <>
       <main>
         <Navbar />
+        <HeroSection />
         <Login />
       </main>
     </>
